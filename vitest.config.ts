0a4cc75a9bcb0fc4aae7072be5b0e7fd67d9e7d6
lvqq <nicksonlvqq@gmail.com>
2022-12-11 17:05:46 +0800

    test: add coverage config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     testTimeout: 20000,
+    coverage: {
+      reporter: ['lcov', 'text'],
+    },
   },
   esbuild: {
     target: 'node14',
